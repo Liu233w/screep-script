d946ae41e270342528fccdc11caa2f69793112f6
Liu's DeskTop <wwwlsmcom@msn.cn>
2019-05-29 02:59:09 +0800

    t

--- src/run.tower.js
+++ src/run.tower.js
@@ -13,22 +13,25 @@ function run() {
         if (item.structureType !== STRUCTURE_TOWER) {
             continue
         }
-
-        const closestDamagedStructure = item.pos.findClosestByRange(FIND_STRUCTURES, FIND_FILTERS.repair(item))
-        if (closestDamagedStructure) {
-            item.repair(closestDamagedStructure)
-        }
-
+        
         const closestHostile = item.pos.findClosestByRange(FIND_HOSTILE_CREEPS)
         if (closestHostile) {
             item.attack(closestHostile)
+            return
         }
-
+        
         const closestDamagedCreep = item.pos.findClosestByRange(FIND_MY_CREEPS, {
             filter: c => c.hits < c.hitsMax,
         })
         if (closestDamagedCreep) {
             item.heal(closestDamagedCreep)
+            return
+        }
+
+        const closestDamagedStructure = item.pos.findClosestByRange(FIND_STRUCTURES, FIND_FILTERS.repair(item))
+        if (closestDamagedStructure) {
+            item.repair(closestDamagedStructure)
+            return
         }
     }
 }
